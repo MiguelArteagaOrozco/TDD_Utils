1f2f3fef44a0d6c93c44cacafe59202bf09caa66
MiguelArteagaOrozco <arteagamiguel@gmail.com>
2016-05-12 13:26:43 -0300

    TestDbAsyncEnumerator

---
+++ EntityFramework/TestDbAsyncEnumerator.cs
@@ -0,0 +1,43 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.Data.Entity;
+using System.Linq;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TDDUtils.EntityFramework
+{
+    internal class TestDbAsyncEnumerator<T> : IDbAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> _inner;
+
+        public TestDbAsyncEnumerator(IEnumerator<T> inner)
+        {
+            _inner = inner;
+        }
+
+        public void Dispose()
+        {
+            _inner.Dispose();
+        }
+
+        public Task<bool> MoveNextAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_inner.MoveNext());
+        }
+
+        public T Current
+        {
+            get { return _inner.Current; }
+        }
+
+        object IDbAsyncEnumerator.Current
+        {
+            get { return Current; }
+        }
+    }
+}
