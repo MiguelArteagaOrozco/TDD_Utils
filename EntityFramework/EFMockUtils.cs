d6b8459767e9a6b773fd028e3550127a4ebf7446
MiguelArteagaOrozco <arteagamiguel@gmail.com>
2016-05-12 13:22:37 -0300

    EFMockUtils

---
+++ EntityFramework/EFMockUtils.cs
@@ -0,0 +1,30 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.Data.Entity;
+using System.Linq;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TDDUtils.EntityFramework
+{
+    public class EFMockUtils
+    {
+        public static DbSet<T> GenerateDBSetubstitude<T>(IQueryable<T> source) where T:class
+        {
+            var entityDBSet = Substitute.For<DbSet<T>, IQueryable<T>, IDbAsyncEnumerable<T>>();
+
+            ((IDbAsyncEnumerable<T>)entityDBSet).GetAsyncEnumerator().Returns(new TestDbAsyncEnumerator<T>(source.GetEnumerator()));
+            ((IQueryable<T>)entityDBSet).Provider.Returns(new TestDbAsyncQueryProvider<T>(source.Provider));
+
+            ((IQueryable<T>)entityDBSet).Expression.Returns(source.Expression);
+            ((IQueryable<T>)entityDBSet).ElementType.Returns(source.ElementType);
+            ((IQueryable<T>)entityDBSet).GetEnumerator().Returns(source.GetEnumerator());
+
+            return entityDBSet;
+        }
+    }
+}
